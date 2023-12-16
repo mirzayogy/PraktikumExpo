a4e314eacc66af254b2aa6595c18174a24dfbcfc
mirzayogy <mirza.yogy@gmail.com>
2023-12-16 21:25:22 +0800

    memanggil API

--- App.js
+++ App.js
@@ -1,10 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+import { Restaurants } from './api/Restaurants';
 
 export default function App() {
+
+  const [restaurants,setRestaurants] = useState(null)
+
+  useEffect(() => {
+    Restaurants().then(result => {
+      console.log(result.data.message)
+      setRestaurants(result.data.restaurants)
+    })
+  },[])
+
+  const renderItem = ({ item }) => {
+    return <Text>{item.name}</Text>
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <FlatList
+        data={restaurants}
+        renderItem={renderItem}
+      />
       <StatusBar style="auto" />
     </View>
   );
